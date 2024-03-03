816851da783a660a920c84a23b94e067503474fe
Muhammad Anfaal <161188326+anfaal1387@users.noreply.github.com>
2024-03-03 07:41:33 +0500

    45 Assignments for Governor House IT Course
    As first assignment was to download node js, git bash etc I mean all the environment creating. So here are the remaining assignments.

---
+++ assaignemnt no3 lower case upper case/main.ts
@@ -0,0 +1,8 @@
+let personname = "Anfaal and Zunair";
+
+console.log(personname.toLowerCase());
+
+console.log(personname.toUpperCase());
+
+console.log(personname.replace(/\b\w/g, (char) => char.toUpperCase()));
+
