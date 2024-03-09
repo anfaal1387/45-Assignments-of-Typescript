45c52ea34f69a2ad875bef5ac767323dc4b6fa7c
Muhammad Anfaal <161188326+anfaal1387@users.noreply.github.com>
2024-03-09 10:59:13 +0500

    45 Assignments given by Governor House IT Initiative
    Have completed one more exercise!

---
+++ Assignment no29/main.ts
@@ -0,0 +1,26 @@
+//Creating an array of my favorite fruits
+let favorite_fruits = ["Mango", "Pomegranate", "Grapes", "Guava", "Water Melon", "Strawberry", "Orange"]
+if(favorite_fruits.includes("Mango")){
+    console.log("I really like Mangoes.");
+}
+if(favorite_fruits.includes("Pomegranate")){
+    console.log("Pomegranate is my favorite fruit.");
+}
+if(favorite_fruits.includes("Grapes")){
+    console.log("Grapes is my favorite fruit.");
+}
+if(favorite_fruits.includes("Guava")){
+    console.log("I raelly love to have guava with after my lunch");
+}
+if(favorite_fruits.includes("Water Melon")){
+    console.log("Water Melon is really good for health.");
+}
+if(favorite_fruits.includes("Strawberry")){
+    console.log("I like to have strawberry milkshake with my breakfast");
+}
+if(favorite_fruits.includes("Orange")){
+    console.log("Orange juice is my favorite one!");
+}
+if(favorite_fruits.includes("Apple")){
+    console.log("Apple is just ok for me.");
+}
